8d44da54ed113b05b4af333dd1cc257d4ef4c884
lSenichl <baryshnikov.semen@mail.ru>
2020-07-05 15:36:27 +0300

    get rest options

--- food_delivery/static/js/main.js
+++ food_delivery/static/js/main.js
@@ -6,11 +6,75 @@ $(document).on('click', '.number-spinner button', function () {
     if (btn.attr('data-dir') == 'up') {
         newVal = parseInt(oldValue) + 1;
     } else {
-        if (oldValue > 1) {
+        if (oldValue > 0) {
             newVal = parseInt(oldValue) - 1;
         } else {
-            newVal = 1;
+            newVal = 0;
         }
     }
     btn.closest('.number-spinner').find('input').val(newVal);
 });
+
+function getAdmArea(data) {
+    let tempArr = [];
+    var selectId = document.getElementById("inputAdmArea");
+    data.forEach(elem => {
+        if (elem.admArea) {
+            if (!tempArr.includes(elem.admArea)) {
+                var createOption = document.createElement("option");
+                createOption.innerHTML = elem.admArea;
+                selectId.appendChild(createOption);
+                tempArr.push(elem.admArea);
+            }
+        }
+    });
+}
+
+function getDistrict(data) {
+    let tempArr = [];
+    var selectId = document.getElementById("inputDistrict");
+    data.forEach(elem => {
+        if (elem.district) {
+            if (!tempArr.includes(elem.district)) {
+                var createOption = document.createElement("option");
+                createOption.innerHTML = elem.district;
+                selectId.appendChild(createOption);
+                tempArr.push(elem.district);
+            }
+        }
+    });
+}
+
+function getTypeObject(data) {
+    let tempArr = [];
+    var selectId = document.getElementById("inputTypeObject");
+    data.forEach(elem => {
+        if (elem.typeObject) {
+            if (!tempArr.includes(elem.typeObject)) {
+                var createOption = document.createElement("option");
+                createOption.innerHTML = elem.typeObject;
+                selectId.appendChild(createOption);
+                tempArr.push(elem.typeObject);
+            }
+        }
+    });
+}
+
+window.onload = function () {
+    const apilink = `http://exam-2020-1-api.std-900.ist.mospolytech.ru/api/data1`;
+    const request = new XMLHttpRequest();
+    const request2 = new XMLHttpRequest();
+    const request3 = new XMLHttpRequest();
+    request.open('GET', apilink);
+    request2.open('GET', apilink);
+    request3.open('GET', apilink);
+
+    request.onload = () => getDistrict(JSON.parse(request.response));
+    request.send();
+
+    request2.onload = () => getAdmArea(JSON.parse(request2.response));
+    request2.send();
+    
+    request3.onload = () => getTypeObject(JSON.parse(request3.response));
+    request3.send();
+}
